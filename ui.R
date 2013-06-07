c6dc5ed01155b472b9022cf33cb16f1646e9b05e
Alex Brown <alexbbrown@apple.com>
2013-06-07 00:05:41 -0700

    convert actionbutton to URL

--- ui.R
+++ ui.R
@@ -7,7 +7,7 @@ urls <- c(
 shinyUI(pageWithSidebar(
 	div(
 		selectInput("url","url",choices=urls,multiple=T,selected=names(urls)[[1]]),
-		actionButton("load_url", "Load URL")),
+		tags$a(href="#", class="action-button shiny-bound-input", id="load_url", "Load URL")),
 	div("sidebar"),
 	div("body",
 	  tableOutput("transferTable"),
