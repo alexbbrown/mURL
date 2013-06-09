701b985657d184dec3d8b114bd57fc84ea1d527e
Alex Brown <alexbbrown@apple.com>
2013-06-08 17:27:00 -0700

    added progress bars and more books

--- server.R
+++ server.R
@@ -25,6 +25,8 @@ new.fetcher <- function(url) {
 
 shinyServer(function(input, output, session) {
 	
+	downloads <- list()
+	
 	murl <- list(
 		multiHandle = getCurlMultiHandle(),
 		multiControl = reactiveValues(
@@ -33,26 +35,14 @@ shinyServer(function(input, output, session) {
 		),
 		# Should have a fetcher for each URL in flight.
 		fetchers = list(),
+		completed = list(),
 		
 		startedCount = 0, # used to help decide to check completeness
 		lastStartedCount = 0, # used to help decide to check completeness
 		lastHandlesRemaining = 0 # used to help decide to check completeness
 	)
 	
-	# detect new url requests and dispatch
-	newUrlObserver <- observe({
-		# doesn't seem to be asynchronous?
-		url <- isolate(input$url) # listen to action button
-		
-		print(file=stderr(),input$load_url)
-		
-		cat(file=stderr(),"adding new URL",url,"\n")
-		
-		# let's 
-		
-		if(is.null(url)||url=="") return(NULL)
-		if(url %in% murl$fetcher$url) return(NULL)
-
+	queue_download <- function(url) {
 		deferred_httr <- GET_deferred(url)
 		new_fetcher <- new.fetcher(url)
 		new_fetcher$deferred_httr <- deferred_httr
@@ -60,45 +50,74 @@ shinyServer(function(input, output, session) {
 		push(murl$multiHandle, deferred_httr$curl)
 
 		murl$fetchers <<- c(murl$fetchers,list(new_fetcher))
-
+		
+		cat(file=stderr(),"count",length(murl$fetchers),"\n")
+		
  		murl$multiControl$complete <- FALSE
 
 		# downloadcount is used to inform any progress monitors and the urlWorker
 		murl$multiControl$downloadCount <<- isolate(murl$multiControl$downloadCount)+1
 		murl$startedCount <- murl$startedCount + 1;
+		
+		new_fetcher
+	}
+	
+	# detect new url requests and dispatch
+	newUrlObserver <- observe({
+		# doesn't seem to be asynchronous?
+		input$load_url # watch the action
+		url <- isolate(input$url) # listen to action button
+				
+		cat(file=stderr(),"adding new URL",url,"\n")
+		
+		if(is.null(url)||url=="") return(NULL)
+		if(url %in% murl$fetcher$url) return(NULL)
+
+		new_download <- queue_download(url)
+		
+		downloads <<- c(list(new_download),downloads)
 	})
 	
 	# URLworker is concerned with the continuing fetch work of the multi
 	# curl handle.  It also tickles completed fetches so they can react.
 	# It currently uses a timer, but could use select in a parallel universe.
 	urlWorker <- observe({
-		cat(file=stderr(),"first look [",isolate(murl$multiControl$downloadCount),"]\n")
+	#	cat(file=stderr(),"first look [",isolate(murl$multiControl$downloadCount),"]\n")
 		
 		murl$multiControl$complete
 		
 		if (length(murl$fetchers)==0||murl$multiControl$complete == TRUE) return(NULL)
 		# do a little more work.  Can we get it to do an intermediate amount of work?
-		cat(file=stderr(),"doing some work [",isolate(murl$multiControl$downloadCount),"]\n")
+	#	cat(file=stderr(),"doing some work [",isolate(murl$multiControl$downloadCount),"]\n")
 		
 		status <- curlMultiPerform(murl$multiHandle, multiple = FALSE)
+
+		if (status$numHandlesRemaining != murl$lastHandlesRemaining ||
+			  murl$startedCount != murl$lastStartedCount) {
+				
+			murl$fetchers <<- Filter(function(fetcher){
+				complete <- with(simpleStatus(isolate(fetcher$deferred_httr$curl)),size.download==content.length.download)
+			
+				if (complete) {
+					# decode content when complete.  this triggers the next step (consumer)
+					fetcher$content <- content(as="text",isolate(fetcher$deferred_httr$response()))
+					pop(murl$multiHandle, isolate(fetcher$deferred_httr$curl))
+					murl$completed <- c(murl$completed, list(fetcher)) # should convert to non-reactive here
+				}
+				return(!all(complete))
+			},murl$fetchers)
+		}
 		
-		if(status$numHandlesRemaining > 0) {
+		murl$lastHandlesRemaining <<- status$numHandlesRemaining
+		murl$lastStartedCount <<- murl$startedCount
+		
+		if (status$numHandlesRemaining > 0) {
 			invalidateLater(1,session)
 			# libcurl knows when curlhandles are complete, but that property is not exported
 			# this code uses the contentlength to detect completeness
 		} else {
 			# all tasks are complete.  Trigger completeness tag
-			# should be more selective - trigger each as they complete.
-			
-			firstFetcher <- murl$fetchers[[1]]
-			
-			completeness <- with(simpleStatus(firstFetcher$deferred_httr$curl),size.download==content.length.download)
-			
-			if (completeness[1]) {
-				# decode content (half-done - needs header)
-				firstFetcher$content <- content(as="text",firstFetcher$deferred_httr$response())					
-			} 
-			
+			# should be more selective - trigger each as they complete.			
 			murl$multiControl$complete <<- TRUE
 		}
 		# downloadcount is used to inform any progress monitors
@@ -106,19 +125,26 @@ shinyServer(function(input, output, session) {
 	})
 	
 	# progress monitor - in the form of a progress table
-	output$transferTable = renderTable({
+	output$transferTable = renderUI({
 		murl$multiControl$downloadCount
-		if (length(murl$fetcher)==0) return(data.frame(a="nothing"))
+		if (length(murl$fetchers)==0) return(div("no downloads"))
 		
-		ldply(murl$fetchers,function(x) {
-			summarize(simpleStatus(x$deferred_httr$curl),
+		z<-do.call(div,
+		llply(murl$fetchers,function(x) {
+			status <- simpleStatus(x$deferred_httr$curl)
+			s<-summarize(status,
 			  url=effective.url,
 			  res=as.character(response.code),
-			  time=total.time,
-			  percent=100*size.download/content.length.download
+			  time=round(total.time),
+			  percent=round(100*size.download/content.length.download)
 			)
+			div(width="100%",style="background-color:gray",
+					div(style=paste0("width:",with(status,round(100*size.download/content.length.download)),"%;","background-color:teal;"),
+					status$effective.url))
 		}
-		)
+		))
+		
+		z
 	})
 	
 	output$hists = renderPlot({
@@ -126,7 +152,7 @@ shinyServer(function(input, output, session) {
 		print(file=stderr(),"Trying to draw\n")
 		if (FALSE==murl$multiControl$complete) return(NULL)
 		print(file=stderr(),"Drawing\n")
-		firstFetcher <- murl$fetchers[[1]]
+		firstFetcher <- downloads[[1]]
 		wordlengths<-function(x)ldply(table(attr(gregexpr("[A-Za-z]+",x)[[1]],"match.length")))
     print(qplot(data=wordlengths(firstFetcher$content),x=as.numeric(as.character(.id)),y=V1,geom="bar",stat="identity"))
 	})
--- ui.R
+++ ui.R
@@ -1,7 +1,11 @@
 require(shiny)
 
 urls <- c(
-	war_and_peace = "http://www.gutenberg.org/files/2600/2600-h/2600-h.htm"
+	`War and peace` = "http://www.gutenberg.org/cache/epub/2600/pg2600.txt",
+	`Huckleberry finn` = "http://www.gutenberg.org/cache/epub/76/pg76.txt",
+	`Beowulf` = "http://www.gutenberg.org/cache/epub/16328/pg16328.txt",
+	`Ulysses` = "http://www.gutenberg.org/cache/epub/4300/pg4300.txt",
+	`Grimm's Fairy Tales` = "http://www.gutenberg.org/cache/epub/2591/pg2591.txt"
 	)
 
 shinyUI(pageWithSidebar(
@@ -10,7 +14,7 @@ shinyUI(pageWithSidebar(
 		tags$a(href="#", class="action-button shiny-bound-input", id="load_url", "Load URL")),
 	div("sidebar"),
 	div("body",
-	  tableOutput("transferTable"),
-	  plotOutput("hists",height=200)
+	  plotOutput("hists",height=200),
+	  uiOutput("transferTable")
 	)
 ))
